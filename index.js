16f20ab8df9542224e8b3e0d913af3ff0d12e354
Thisseas Xanthopoulos <thisseasx@yahoo.com>
2020-11-06 20:14:22 +0200

    Rewrite for memory efficiency (unstable)

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const uuid = require('uuid');
-const { flow, filter, map } = require('lodash/fp');
 
 const PORT = process.env.PORT || 3000;
 const PEER_PORT = process.env.PEER_PORT;
@@ -11,32 +10,23 @@ const PEER_SECURE = process.env.PEER_SECURE;
 
 app
   .set('view engine', 'ejs')
+  .get('/', (_, res) => {
+    res.redirect(`/${uuid.v4()}`);
+  })
   .get('/:roomId', (req, res) => {
     const roomId = req.params.roomId;
     res.render('index', { roomId, PEER_PORT, PEER_HOST, PEER_SECURE });
-  })
-  .get('/', (req, res) => {
-    res.redirect(`/${uuid.v4()}`);
   });
 
 io.on('connect', socket => {
-  socket.on('new-peer', (peerId, roomId) => {
-    socket.peerId = peerId;
-    socket.roomId = roomId;
+  socket.on('join-room', (roomId, userPeerId) => {
+    socket.join(roomId, () => {
+      socket.to(roomId).emit('user-joined', userPeerId);
 
-    socket.join(roomId);
-    socket.to(roomId).emit('peer-joined', peerId);
-
-    const allPeersInRoom = flow(
-      filter({ roomId }),
-      map('peerId'),
-    )(io.sockets.sockets);
-
-    socket.emit('all-peers-in-room', allPeersInRoom);
-  });
-
-  socket.on('disconnect', () => {
-    socket.to(socket.roomId).emit('peer-left', socket.peerId);
+      socket.on('disconnect', () => {
+        socket.to(roomId).emit('user-left', userPeerId);
+      });
+    });
   });
 });
 
