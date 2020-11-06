0b31b787b7cb38e0bcb2fd9553527df58ab57faa
Thisseas Xanthopoulos <thisseasx@yahoo.com>
2020-11-06 17:21:59 +0200

    Add room functionality

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const uuid = require('uuid');
+const { flow, filter, map } = require('lodash/fp');
 
 const PORT = process.env.PORT || 3000;
+const PEER_PORT = process.env.PEER_PORT;
+const PEER_HOST = process.env.PEER_HOST;
+const PEER_SECURE = process.env.PEER_SECURE;
 
-app.set('view engine', 'ejs').use('/', (_, res) => {
-  res.render('index');
-});
+app
+  .set('view engine', 'ejs')
+  .get('/:roomId', (req, res) => {
+    const roomId = req.params.roomId;
+    res.render('index', { roomId, PEER_PORT, PEER_HOST, PEER_SECURE });
+  })
+  .get('/', (req, res) => {
+    res.redirect(`/${uuid.v4()}`);
+  });
 
 io.on('connect', socket => {
-  socket.on('new-peer', peerId => {
-    socket.broadcast.emit('peer-joined', peerId);
+  socket.on('new-peer', (peerId, roomId) => {
     socket.peerId = peerId;
+    socket.roomId = roomId;
 
-    const allPeers = Object.entries(io.sockets.sockets).map(
-      ([_, sock]) => sock.peerId,
-    );
+    socket.join(roomId);
+    socket.to(roomId).emit('peer-joined', peerId);
 
-    socket.emit('all-peers', allPeers);
+    const allPeersInRoom = flow(
+      filter({ roomId }),
+      map('peerId'),
+    )(io.sockets.sockets);
+
+    socket.emit('all-peers-in-room', allPeersInRoom);
   });
 
   socket.on('disconnect', () => {
-    socket.broadcast.emit('peer-left', socket.peerId);
+    socket.to(socket.roomId).emit('peer-left', socket.peerId);
   });
 });
 
